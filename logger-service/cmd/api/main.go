45b99e227f5f0026ce748bc53b766240736b3b53
PedroHolo <holodniakpedro@gmail.com>
2023-05-29 22:26:21 -0300

    feat: initial with logger service

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -1,4 +1,16 @@
-package api
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log-service/data"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 const (
 	webPort  = "80"
@@ -7,6 +19,65 @@ const (
 	grpcPort = "50001"
 )
 
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
 func main() {
+
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	//start web server
+	go app.server()
+}
+
+func (app *Config) server() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic()
+	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	//create a connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	//connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Panic("Error connecting to MongoDB", err)
+		return nil, err
+	}
+
+	return c, nil
 
 }
